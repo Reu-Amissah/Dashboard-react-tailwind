c545d0ba0630ee3c007d70e03fa345f3895f0c72
Reu-Amissah <reujoyamissah@gmail.com>
2023-07-08 00:30:57 +0000

    add chart js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,5 +1,95 @@
 import React, { useState } from "react";
 import logo from "../assets/226411.png";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+
+import { Doughnut } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  // Other chart options
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const Ddata = {
+  labels: ["Net Revenue", "Gross Income", "Marketing"],
+  datasets: [
+    {
+      data: [15, 5, 8],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+      ],
+    },
+  ],
+};
+
+export const Ldata = {
+  labels,
+  datasets: [
+    {
+      data: [35, 2, 10, 6, 2, 3, 70],
+      lineTension: 0.4,
+      pointRadius: 0,
+      // labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Patient",
+      data: [12, 19, 3, 5, 2, 3, 24],
+      lineTension: 0.4,
+      pointRadius: 0,
+      // labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Appointment",
+      lineTension: 0.4,
+      pointRadius: 0,
+      data: [16, 20, 1, 1, 8, 9, 20],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
 
 const Dashboard = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -248,6 +338,26 @@ const Dashboard = () => {
               </span>
               <p className="text-green-500 pr-1">3.5% </p>
               <p> vs 3 weeks report</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-between space-x-4 mt-5">
+          <div className="w-2/4 border border-slate-200 p-3 rounded-lg h-2/5">
+            <div className="text-md">Total Progress</div>
+            <div className="h-full">
+              <Line data={data} options={options} />
+            </div>
+          </div>
+          <div className="w-1/4 border border-slate-200 p-3 rounded-lg h-4/6">
+            <div className="text-md">Total Progress</div>
+            <div className="h-full">
+              <Doughnut data={Ddata} />
+            </div>
+          </div>
+          <div className="w-1/4 border border-slate-200 p-3 rounded-lg h-5/6">
+            <div className="text-md">Total Progress</div>
+            <div className="h-full">
+              <Line data={Ldata} options={options} />
             </div>
           </div>
         </div>
